5fcf5e73123405874c259af06d8e3367efca8303
anacarolinaferr <anacarolina.ferreira@mercadolivre.com>
2021-09-02 14:17:09 -0300

    Commit Idade de uma pessoa

---
+++ SPRING-BOOT/Spring-IdadePessoa/src/main/java/com/idadepessoa/SpiIdadePessoaApplication.java
@@ -0,0 +1,13 @@
+package com.idadepessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiIdadePessoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpiIdadePessoaApplication.class, args);
+    }
+
+}
---
+++ SPRING-BOOT/Spring-IdadePessoa/src/test/java/com/idadepessoa/SpiIdadePessoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idadepessoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiIdadePessoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
